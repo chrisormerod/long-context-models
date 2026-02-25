56c1ccb13c697bc99e4a8b8d2a313f3dc02726f4
chrisormerod <cormerod@collegeboard.org>
2026-02-24 20:27:06 -0500

    a

--- data/persuade.py
+++ data/persuade.py
@@ -5,8 +5,8 @@ Created on Tue Feb 24 19:23:56 2026
 
 @author: cormerod
 """
-import os
-os.environ['CURL_CA_BUNDLE'] = '/home/cormerod/pem/Zscaler-AWS-Feb2025.pem'
+#import os
+#os.environ['CURL_CA_BUNDLE'] = '/home/cormerod/pem/Zscaler-AWS-Feb2025.pem'
 from datasets import load_dataset
 
 dataset_path = "ruudra1/PERSUADE"
