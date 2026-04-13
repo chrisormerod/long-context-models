73487ea61fa86bb2d7bed9a45f2bb7838401f6cc
chrisormerod <cormerod@collegeboard.org>
2026-04-13 11:24:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ tokenizer = AutoTokenizer.from_pretrained(model_id)
 #     L.mixer.A_log.requires_grad_(False)
 #     L.mixer.D.requires_grad_(False)
     
-for x in model.backbone.layers: x.requires_grad_(False)
-for x in model.backbone.layers: x.mixer.x_proj.requires_grad_(True)
-for x in model.backbone.layers: x.mixer.dt_proj.requires_grad_(True)
-for x in model.backbone.layers: x.mixer.in_proj.requires_grad_(True)
-for x in model.backbone.layers: x.mixer.out_proj.requires_grad_(True)
+# for x in model.backbone.layers: x.requires_grad_(False)
+# for x in model.backbone.layers: x.mixer.x_proj.requires_grad_(True)
+# for x in model.backbone.layers: x.mixer.dt_proj.requires_grad_(True)
+# for x in model.backbone.layers: x.mixer.in_proj.requires_grad_(True)
+# for x in model.backbone.layers: x.mixer.out_proj.requires_grad_(True)
 
 trainer, train_result, eval_metrics = train(model, tokenizer, 
                                              data['train'].rename_columns({"full_text":"text","score":"label"}), 
