e5bfa4148a3ca799d9a87385c68d315e7bfdf2a1
chrisormerod <cormerod@collegeboard.org>
2026-04-13 11:02:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ model_id = "state-spaces/mamba-130m-hf"
 
 data = get_asap(DATA_DIR)
 model = MambaForSequenceClassification.from_pretrained(model_id, num_labels=7)
-model.classifier = torch.nn.Linear(in_features = 768,out_features=7, bias=False)
+# model.classifier = torch.nn.Linear(in_features = 768,out_features=7, bias=False)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 # for L in model.mamba.layers:
 #     L.mixer.A_log.requires_grad_(False)
 #     L.mixer.D.requires_grad_(False)
     
-for x in model.mamba.layers: x.requires_grad_(False)
-for x in model.mamba.layers: x.mixer.x_proj.requires_grad_(True)
-for x in model.mamba.layers: x.mixer.dt_proj.requires_grad_(True)
-for x in model.mamba.layers: x.mixer.in_proj.requires_grad_(True)
-for x in model.mamba.layers: x.mixer.out_proj.requires_grad_(True)
+for x in model.backbone.layers: x.requires_grad_(False)
+for x in model.backbone.layers: x.mixer.x_proj.requires_grad_(True)
+for x in model.backbone.layers: x.mixer.dt_proj.requires_grad_(True)
+for x in model.backbone.layers: x.mixer.in_proj.requires_grad_(True)
+for x in model.backbone.layers: x.mixer.out_proj.requires_grad_(True)
 
 trainer, train_result, eval_metrics = train(model, tokenizer, 
                                              data['train'].rename_columns({"full_text":"text","score":"label"}), 
